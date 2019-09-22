c7d041d7c71cd82dd7d0012d7681a3c8e484639a
Lucy <luciamongeg@gmail.com>
2019-09-22 09:56:09 -0600

    Conección con servidor, envia json

--- ArduinoESP8266.ino
+++ ArduinoESP8266.ino
@@ -1,17 +1,18 @@
 #include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
 #include <ESP8266HTTPClient.h>
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println();
 
   WiFi.begin("Nontraf", "jmmg1403");
-
+  
   Serial.print("Conectando");
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
+    delay(5000);
     Serial.print(".");
   }
   Serial.println();
@@ -25,13 +26,28 @@ void setup()
 void loop() {
   
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
-    HTTPClient Post;
-    Post.begin("http://192.168.1.76:8080"); 
-    Post.addHeader("Content-type", "application/json");
- 
-    Post.POST("{Key:984984}");
-    String payload = Post.getString();
-    Post.end();
+    StaticJsonBuffer<300> JSONbuffer;
+    JsonObject& JSONencoder = JSONbuffer.createObject();
+
+    JSONencoder["temperatura"] = "muyCaliente";
+
+    char JSONmessageBuffer[300];
+    JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+    Serial.println(JSONmessageBuffer);
+
+
+    HTTPClient client;
+    client.begin("http://192.168.1.76:8080"); 
+    client.addHeader("Content-type", "application/json");
+
+    int httpCode = client.POST(JSONmessageBuffer); //Send the request
+    String payload = client.getString(); //Get the response payload
+
+    Serial.println(httpCode); //Print HTTP return code
+    Serial.println(payload); //Print request response payload
+    
+    client.end(); //Close connection
+
     Serial.println("Si entro.....");
   } 
   delay(2000);  
